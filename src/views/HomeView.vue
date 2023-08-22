63bb09782cc77e4f07f6d0b1a87441cea884f80e
Antonio Costa <akcosta@pdmfc.com>
2023-08-22 12:22:02 +0100

    chore: update home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,14 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <div class="bg-diagonal z-0"></div>
+    <div class="max-w-screen-lg my-0 mx-auto flex justify-between p-6 gap-6 flex-col z-10 h-full sm:flex-row">
+      <PostList />
+      <History />
+    </div>
+  </div>
 </template>
+
+<script setup lang="ts">
+  import PostList from '@/components/PostList.vue'
+  import History from '@/components/History.vue'
+</script>
